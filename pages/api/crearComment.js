0aee631334986a7423888739cc38e22a6a6c1e36
Manuel Gonzalez <manuelg9704@gmail.com>
2023-08-14 18:11:23 -0500

    redplot2

--- pages/api/crearComment.js
+++ pages/api/crearComment.js
@@ -12,6 +12,7 @@ const createComment = async (req, res) => {
 
       res.status(201).json({ message: 'Comentario creado exitosamente' });
     } catch (error) {
+      console.error("Error al crear comentario:", error.message);
       res.status(500).json({ message: 'Hubo un error al crear el comentario' });
     }
   } else {
